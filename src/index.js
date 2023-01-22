d7a83b003d0e268f322bce856531145c831a9d9b
Alexander Kharmansky <davilex_x@vk.com>
2023-01-22 17:03:03 +0600

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const bracketsObj = {};
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    let openBracket = bracketsConfig[i][0];
+    let closeBracket = bracketsConfig[i][1];
+    bracketsObj[openBracket] = closeBracket;
+  }
+
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    let bracket = str[i];
+    let lastBracketInStack = stack[stack.length - 1];
+
+    if (bracketsObj[lastBracketInStack] === bracket && stack.length === 0) {
+      return false;
+    }
+
+    if (bracketsObj[lastBracketInStack] !== bracket) {
+      stack.push(bracket);
+    } else {
+      stack.pop();
+    }
+  }
+
+  if (stack.length === 0) {
+    return true;
+  }
+  return false;
+};
